9f0d036fb2a82a55193f08187bf6accfc6c43c0b
Bool Fin <boolfin@gmail.com>
2025-05-10 16:45:16 +0000

    do it

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -36,10 +36,11 @@ export default function AppLayout({ children }: PropsWithChildren) {
           <SidebarNav />
         </SidebarContent>
         <SidebarFooter className="p-2 mt-auto">
-            {/* Example Footer Item */}
-            <Button variant="ghost" className="w-full justify-start group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:p-0">
-                <Settings className="h-5 w-5 mr-3 group-data-[collapsible=icon]:mr-0" />
-                <span className="truncate group-data-[collapsible=icon]:hidden">Settings</span>
+            <Button variant="ghost" className="w-full justify-start group-data-[collapsible=icon]:justify-center group-data-[collapsible=icon]:size-8 group-data-[collapsible=icon]:p-0" asChild>
+                <Link href="/settings">
+                    <Settings className="h-5 w-5 mr-3 group-data-[collapsible=icon]:mr-0" />
+                    <span className="truncate group-data-[collapsible=icon]:hidden">Settings</span>
+                </Link>
             </Button>
         </SidebarFooter>
       </Sidebar>
